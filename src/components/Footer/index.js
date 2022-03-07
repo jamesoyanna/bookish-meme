b68ad11e40e35e98c80b641d59802d8e900ad2ec
James <engrjayt200@gmail.com>
2022-03-07 17:03:29 +0100

    Add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+const Index = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <div className='container'>
+                    <span className='copyright'>&copy; User Info App</span>
+                    </div>
+                </footer>
+        </div>
+    );
+}
+
+export default Index;
